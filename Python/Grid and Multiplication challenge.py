97cdddb6d2549c7961c29acc89f9e058ed85b091
fmssf <43239072+fmssf@users.noreply.github.com>
2021-06-08 13:19:54 -0400

    python

--- Python/Grid and Multiplication challenge.py
+++ Python/Grid and Multiplication challenge.py
@@ -1,5 +1,6 @@
-import numpy
-#from img import *
+import numpy,pandas,time,copy
+from img import *
+
 '''create 3 arrays, one to hold temp items coming in, one to hold parent number
 and one to hold child numbers.'''
 textFile = open("numbers.txt","r")
@@ -8,39 +9,42 @@ lines = textFile.readlines()
 tmp = []
 parent = []
 child = []
-
 '''
 The for loop below will compare the current row with the row above it.
 The following snipit of code will prepare row below before the loop starts.
-It reads in and turns the entries from strings to numbers'''
+It reads in and turns the entries from strings to numbers
+- grabs the last line in the txt file
+'''
 tmp = lines[len(lines)-1].strip().split(' ')
 for i in tmp:
   child.append(int(i))
 
-  # Walk through every row of the triangle except the last.  The last was prebuilt
-#show the output
-#print(len(tmp)-1)
-for row in range(13,0,-1):
-    tmp = lines[row].strip().split(' ')
-    parent.append(int(i))
-    for j in range(0,len(tmp),1):
-        if child[j] > child[j+1]:
-          parent[j] = child[j] +parent[j]
-        else:
-          child[j] = parent[j] +child[j]
-        child = len(parent);
-        parent.insert(child, 0);
-        print(child)
-        break;
-
-        
+''' walk the throug the text file from the bottom up. start with the second line from the bottom.'''
+for i in range(-2,0):
+  
+  #grabs a line and add it to the parent
+  tmp = lines[i].strip().split(' ')
+  for j in tmp:
+    parent.append(int(j))
     
-            
+  for j in range(0,len(child),-1):
+    if child[j] > child[j+1]:
+      parent[j] += child[j]
+    else:
+      parent[j] += child[j+1]
+  childNums = int(len(tmp));
+  print(copy.copy(parent[i]))
+
 '''
-for ()  
+  Walk through every row of the triangle except the last. The last was prebuilt
+'''
+
+
+
+'''
             {
                 # Grab the next row up the triangle as the parent
-               
+               +
 
 
                 # Walk through each item in the parent and add the largest adjacent item from the child

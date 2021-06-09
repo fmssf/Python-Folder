b25eff69b62342c472f984a7df2770ff6d7ec9c7
fmssf <43239072+fmssf@users.noreply.github.com>
2021-06-09 12:30:01 -0400

    Update Palendrome.py

--- Python/Palendrome/Palendrome.py
+++ Python/Palendrome/Palendrome.py
@@ -8,16 +8,16 @@ for i in range(100,1000):
 
 
   for j in range(i,1000):
-    equales = i * j 
-
-    output = i,"*",j,"=",equales
+    equales = i * j
+    output = str(equales)
     revers = output[::-1]
     if revers == output:
       pallendrome.append(revers)
+      print(pallendrome)
     else:
       notPallendrome.append(output)
+print(notPallendrome,end="")
     
-    print(i)
 '''
 for j in range(i,1000):
     equales = i * j 

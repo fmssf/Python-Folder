253523fd60c27a573e2aad2527b459c23d26e134
fmssf <43239072+fmssf@users.noreply.github.com>
2021-06-09 08:53:02 -0400

    palendrome

--- Python/Palendrome/Palendrome.py
+++ Python/Palendrome/Palendrome.py
@@ -1,4 +1,6 @@
+import random
 from img import *
 
-num1 = int(input("input your first number\n >>> "))
- 
+num1 = int(random.randint(111,999))
+num2 = int(random.randint(111,999))
+print(num1, "*",num2 , "=" ,num1 * num2)
